43bd0db27a6399c0088785f7d3f87635604ca36b
Mahmut P <109034196+C0derByM4H6301@users.noreply.github.com>
2022-12-27 19:16:03 +0300

    Add files via upload

---
+++ dene 2.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+import os
+import cv2
+
+print("helloo world!")
+# Counter-Strike 1.6'yı açın "aklında bulunsun" \common\Counter-Strike\hl.exe
+os.startfile("Discord.lnk")
